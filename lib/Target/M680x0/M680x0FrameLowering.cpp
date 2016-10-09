3c56c98e3d1e27abd655c9c08ffc375616ed2225
Artyom Goncharov <m4yers@gmail.com>
2016-10-09 18:32:11 +0300

    [M680x0] Fix Stack Offset Area

--- lib/Target/M680x0/M680x0FrameLowering.cpp
+++ lib/Target/M680x0/M680x0FrameLowering.cpp
@@ -32,7 +32,7 @@ using namespace llvm;
 
 M680x0FrameLowering::
 M680x0FrameLowering(const M680x0Subtarget &STI, unsigned Alignment)
-  : TargetFrameLowering(StackGrowsDown, Alignment, 0, Alignment),
+  : TargetFrameLowering(StackGrowsDown, Alignment, -4, Alignment),
       STI(STI), TII(*STI.getInstrInfo()), TRI(STI.getRegisterInfo()) {
   SlotSize = STI.getSlotSize();
   StackPtr = TRI->getStackRegister();
